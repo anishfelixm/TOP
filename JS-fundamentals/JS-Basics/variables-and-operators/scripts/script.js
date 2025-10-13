55d4fecfbc028a6f276eeb4901db827be6eecb40
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-13 21:41:59 +0530

    feat : completed assignment

--- JS-fundamentals/JS-Basics/variables-and-operators/scripts/script.js
+++ JS-fundamentals/JS-Basics/variables-and-operators/scripts/script.js
@@ -11,3 +11,24 @@ console.log(age);
 
 const pi = 3.14;
 console.log(pi);
+
+console.log((3+2) - 76*(1+1));
+
+//1
+console.log(23+97);
+//2
+console.log(1+2+3+4+5+6);
+//3
+console.log((4+6+9)/77);
+//4
+let a = 10;
+console.log(a);
+a=20;
+console.log(a);
+let b = 7*a;
+console.log(b);
+//5
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+console.log(percentage);
