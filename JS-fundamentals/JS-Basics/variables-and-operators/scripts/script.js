40eae730b2a4399a024d8ca662bf33d10f220cec
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-14 22:05:13 +0530

    feat: read about numbers

--- JS-fundamentals/JS-Basics/variables-and-operators/scripts/script.js
+++ JS-fundamentals/JS-Basics/variables-and-operators/scripts/script.js
@@ -10,7 +10,7 @@ age = 40;
 console.log(age);
 
 const PI = 3.14;
-console.log(pi);
+console.log(PI);
 
 console.log((3+2) - 76*(1+1));
 
@@ -18,6 +18,38 @@ let one = 1,
     two = 2;
 alert(one);
 alert(two);
+
+let x = 3;
+x++;
+let z = x ** 2;
+alert(z);
+
+let a1 = 10;
+let b1 = " apples";
+console.log(a1+b1);
+
+console.log(10+20+"30");
+
+let wa2 = 100 / "Apple";
+isNaN(wa2);
+
+let a3 = 2;
+while (a3 != Infinity) {
+    a3 = a3 ** a3;
+}
+
+console.log(-2 / 0);
+console.log(typeof Infinity);
+
+let a4 = 32;
+console.log(a4.toString(2));
+
+let a5 = new Number(23);
+
+let a6 = 500;
+let a7 = new Number(500);
+console.log(a6 == a7);
+console.log(a6 === a7);
 
 //1
 console.log(23+97);
