1e2b22f994e8c0cb8cf64f8b379558c9a51f3814
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-19 02:41:18 +0530

    feat: conditionals in JS

--- JS-fundamentals/JS-Basics/variables-and-operators/scripts/script2.js
+++ JS-fundamentals/JS-Basics/variables-and-operators/scripts/script2.js
@@ -30,3 +30,23 @@ console.log(`Is 5 greater than 4: ${5 > 4}`);
 const comp = 'Z' > 'A';
 console.log(`Is Z > A : ${comp}`);
 console.log("null === undefined : " + (null === undefined));
+
+let hour = 12;
+let isWeekend = true;
+if (hour < 10 || hour > 18 || isWeekend) {
+  console.log( 'The office is closed.' );
+}
+
+console.log(undefined || null || 0); // undefined truth value is false, null truth value is false, 0 is false and the value is returned as 0 of this expression bcz it's the last operand.
+true || console.log("not printed");
+false || console.log("printed");
+console.log( null || 2 && 3 || 4 ); // chain of OR will return ffirst truthy value. but due to && precedence higher 2&&3 is first calculated which returns 3 following the chaining of &&. then the chaining of OR is calculated which returns 3 as it's the first truthy value.
+
+
+let text;
+if (Math.random() < 0.5) {
+  text = "<a href='https://w3schools.com'>Visit W3Schools</a>";
+} else {
+  text = "<a href='https://wwf.org'>Visit WWF</a>";
+}
+document.getElementById("demo").innerHTML = text;
